86093da163d1561f295dca9737ffe8510957542d
Aman Bhimani <amanb014@gmail.com>
2018-06-13 19:58:53 -0400

    Personalized home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Aman Bhimani',
+    intro: {
+      name: 'Aman Bhimani',
+      title: 'Software Developer',
+      bio: 'Hello! I\'m Aman and I\'m currently in the field of change. Don’t understand? In layman’s terms, it’s called Software. It is always changing, and we just have to embrace the change in software daily. I have been intrigued by software since the day I put my hands on my first trackball-powered mouse. Surely I wasn’t developing at the age of 6 on my Windows 95 machine; however, I was very interested in the inner workings of my desktop computer. Figuring out software problems, was my niche. When the computer was inoperable, the mechanic came by and revealed the spaghetti mess that was in my computer. Since that day, I have been interested in the world of creation, improvement, and learning, which is Software Engineering.'
+    }
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass'
   ],
 }
+
+/**
+ *     intro: {
+      name: 'Aman Bhimani',
+      title: 'Software Developer',
+      bio: 'Hello! I\'m Aman and I\'m currently in the field of change. Don’t understand? In layman’s terms, it’s called Software. It is always changing, and we just have to embrace the change in software daily. I have been intrigued by software since the day I put my hands on my first trackball-powered mouse. Surely I wasn’t developing at the age of 6 on my Windows 95 machine; however, I was very interested in the inner workings of my desktop computer. Figuring out software problems, was my niche. When the computer was inoperable, the mechanic came by and revealed the spaghetti mess that was in my computer. Since that day, I have been interested in the world of creation, improvement, and learning, which is Software Engineering.'
+    }
+ */
---
+++ src/components/intro.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Intro = (data) => {
+	return (
+		<div className="intro-container">
+
+			<h4>/* Hello World! */</h4>
+
+			<div className="item">
+				<div>01</div>
+				<div>name =</div>
+				<div>Aman Bhimani</div>
+			</div>
+	
+			<div className="item">
+				<div>02</div>
+				<div>title =</div>
+				<div>Software Developer</div>
+			</div>
+
+			<div className="item">
+				<div>03</div>
+				<div>bio =</div>
+				<div>Hello! I'm Aman and I'm currently in the field of change. Don’t understand? In layman’s terms, it’s called Software. It is always changing, and we just have to embrace the change in software daily. I have been intrigued by software since the day I put my hands on my first trackball-powered mouse. Surely I wasn’t developing at the age of 6 on my Windows 95 machine; however, I was very interested in the inner workings of my desktop computer. Figuring out software problems, was my niche. When the computer was inoperable, the mechanic came by and revealed the spaghetti mess that was in my computer. Since that day, I have been interested in the world of creation, improvement, and learning, which is Software Engineering.</div>
+			</div>
+		</div>
+	)
+}
+
+export default Intro
