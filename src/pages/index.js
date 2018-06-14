4c563ca57b41738445f15283a3a99fcb3c87977d
Aman Bhimani <amanb014@gmail.com>
2018-06-13 21:03:53 -0400

    Dynamic content from graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,22 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Intro from './../components/intro'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <div>
-    <Intro />
+    <Intro content={data.site.siteMetadata.intro}/>
   </div>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query SiteIntroContent {
+    site {
+      siteMetadata {
+        intro {
+          name, title, bio
+        }
+      }
+    }
+  }
+`
